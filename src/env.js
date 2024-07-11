2b13b8ffefabade3634dc9c33c989e08e00f2696
Emirsyah <muhammademir48@gmail.com>
2024-07-11 23:40:39 +0700

    feat: add shadcn, integrate prisma supabase

--- src/env.js
+++ src/env.js
@@ -20,6 +20,8 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    // NEXT_PUBLIC_SUPABASE_URL: z.string(),
+    // NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
   },
 
   /**
@@ -29,6 +31,10 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    // NODE_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    // NODE_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    // DATABASE_URL: process.env.DATABASE_URL,
+    // NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
